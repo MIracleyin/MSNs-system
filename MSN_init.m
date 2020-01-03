4cb29198004503944f4e307e1b7391623a1c5525
MIracleyin <miracleyin@live.com>
2020-01-03 12:07:57 +0800

    simulation
    area setup

--- MSN_init.m
+++ MSN_init.m
@@ -1,7 +1,19 @@
 %%%%%%%%%%%%%%%% MSN system %%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%% MSN_init.m %%%%%%%%%%%%%%%%%%%%%%%%
 % This script is used to simulate the MSN system %%%
 % from Pitiphol Pholpabu and Lie-Liant Yang .%%%%%%%
 % This system depends on RPM model. %%%%%%%%%%%%%%%%
 % The proposed model can intergate people's roles, %
 % daily activies and occasional acitivies. %%%%%%%%%
 % The part of routing protocols will be later. %%%%%
+
+clc;
+clear all;
+close all;
+close all hidden;
+
+%% Simulation Setup
+
+% Simulation area
+Length = 1000; %(m)
+Width = 1000; %(m)
